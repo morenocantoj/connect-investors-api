df5904369038013b94a7ad2de2f49a6174973c16
morenocantoj <morenocantoj@gmail.com>
2019-02-15 17:53:13 +0100

    Initialized express server and setted Babel compiler for server

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+// Main route
+app.get('/', (req, res) => {
+  res.send('Initial setting!')
+})
+
+app.listen(8000, () => {
+  console.log('Server running...')
+})
